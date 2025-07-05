c8d0a083e5e9e3413406eac4bfae14d188075a58
Yasmine Abid <147927836+yasmineAbid@users.noreply.github.com>
2025-07-05 14:47:12 +0100

    🔧 Mise en place gateway dynamique et correction Eureka

--- medecin/src/main/java/com/hopital/medecin/MedecinApplication.java
+++ medecin/src/main/java/com/hopital/medecin/MedecinApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 @SpringBootApplication
 @EnableFeignClients
+
 public class MedecinApplication {
 
 	public static void main(String[] args) {
